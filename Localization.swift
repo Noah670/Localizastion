5ca88c26abd2b44eb07355551d67f8883b92c953
Noah <noahrules96950@gmail.com>
2020-04-30 17:01:03 +1000

    Add files via upload

---
+++ Localization.swift
@@ -0,0 +1,40 @@
+// 4-30-2020
+// Noah670
+
+print("Swift has some pretty cool localization support!")
+
+
+var someNumber = 69
+
+let myName: String = "Noah"
+
+print("check this out")
+let fullName = "My name is: \(myName)"
+print(fullName)
+
+print("My favourite number is \(someNumber)")
+
+var languageSupport  = "母语支持很棒"
+print(languageSupport)
+
+var JapanCapital = "東京は日本の首都です"
+print(JapanCapital)
+print()
+
+var game = "Death Stranding"
+var gameCost = 60.00
+var gameProducer = "Hideo Kojima"
+var gameReleaseDate = ("November 8th, 2019")
+
+print("The game \(game) retail cost is only \(gameCost)!")
+print("\(game) was produced by \(gameProducer) and released on \(gameReleaseDate)")
+print()
+
+
+var CyberGame = "Cyberpunk 2077"
+var CyberDeveloper = "CD Projekt RED"
+var CyberRelease = "September 17, 2020"
+var CyberPlatforms = "PC, Playstation 4, and Xbox One"
+var CyberGenre = "role-playing first person shooter"
+
+print(" \(CyberGame) is an upcoming \(CyberGenre) developed by \(CyberDeveloper) and will be released on \(CyberRelease) for \(CyberPlatforms)")
